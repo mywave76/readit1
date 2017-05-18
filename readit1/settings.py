4e3dcd3745650a79d81f9dc931e807c9571accea
mywave76 <mywave.reloaded@yahoo.com>
2017-05-18 11:00:39 +0200

    HTML templates with extends and include

--- readit1/settings.py
+++ readit1/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'readit1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'readit1', 'templates',)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
